a0b1a839d90ab98701a3c0a6b883640fec444033
Martin Melendez <martin.melendez@flexxus.net.ar>
2024-03-07 10:36:16 -0300

    00_intro

---
+++ 00_intro/activity.js
@@ -0,0 +1,16 @@
+const nombre = "pedro";
+let edad = 30;
+const precio = 10;
+const nombreSeries ="stranger things";
+const peliculas = ["pokemon","superman","los caballeros del zodiaco"];
+
+edad = edad + 1;
+edad += 1;
+edad++;
+
+console.log(nombre);
+console.log(edad);
+console.log(precio);
+console.log(nombreSeries);
+console.log(peliculas);
+
