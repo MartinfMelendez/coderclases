e9005a8fa2ecb58ffd3ee0509c4943479a671152
MartinfMelendez <martinf.melendez@gmail.com>
2024-03-25 14:10:30 -0300

    06_express

---
+++ 06_express/data/fs/NotesManager.js
@@ -0,0 +1,110 @@
+import fs from "fs";
+import crypto from "crypto";
+
+class NotesManagers {
+  constructor() {
+    this.path = "./data/fs/files/notes.json"; //ruta donde se crea el archivo
+    this.init();
+  }
+  init() {
+    const exists = fs.existsSync(this.path);
+    if (!exists) {
+      const stringData = JSON.stringify([], null, 2);
+      fs.writeFileSync(this.path, stringData);
+      console.log("Archivo creada");
+    } else {
+      console.log("archivo ya existe");
+    }
+  }
+  async create(data) {
+    try {
+      if (!data.text) {
+        const error = new Error("ingrese un texto");
+        throw error;
+      } else {
+        const note = {
+          id: crypto.randomBytes(12).toString("hex"),
+          text: data.text,
+          category: data.category || "to do",
+          date: data.date || new Date(),
+        };
+        let all = await fs.promises.readFile(this.path, "utf-8");
+        all = JSON.parse(all);
+        all.push(note);
+        all = JSON.stringify(all, null, 2);
+        await fs.promises.writeFile(this.path, all);
+        console.log("Nota creada");
+        return note;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+  async read() {
+    try {
+      let all = await fs.promises.readFile(this.path, "utf-8");
+      all = JSON.parse(all);
+      //all = all.filter(each => each.category===cat) cat = "done"
+      if (all.length === 0) {
+        //throw new Error("NO HAY NOTAS")
+        return null
+      } else{
+        console.log(all);
+        return all;
+      }
+      
+    } catch (error) {
+      throw error;
+    }
+  }
+  async readOne(id) {
+    try {
+      let all = await fs.promises.readFile(this.path, "utf-8");
+      all = JSON.parse(all);
+      let one = all.find((each) => each.id === id);
+      if (!one) {
+        throw new Error("No encontrado");
+      } else {
+        console.log(one);
+        return one;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async destroy(id) {
+    try {
+      let all = await fs.promises.readFile(this.path, "utf-8");
+      all = JSON.parse(all);
+      let one = all.find((each) => each.id === id);
+      if (!one) {
+        throw new Error("No EXISTE");
+      } else {
+        let filtered = all.filter((each) => each.id !== id);
+        filtered = JSON.stringify(filtered, null, 2);
+        await fs.promises.writeFile(this.path, filtered);
+        console.log("Nota eliminada");
+        console.log(one);
+        return one;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+/*
+async function test() {
+  try {
+    const notes = new NotesManagers();
+    //notes.create({ text: "Mi primera nota" });
+    //notes.read();
+    //notes.readOne("538c09aa432b1aac00d77d2c");
+    //notes.readOne("2983152a85");
+    notes.destroy("c37cf5eeae3014303c465544");
+  } catch (error) {
+    console.log(error);
+  }
+}
+*/
+const notesManager = new NotesManagers();
+export default notesManager;
---
+++ 06_express/server.js
@@ -0,0 +1,120 @@
+import express, { response } from "express";
+//import nombreModulo from "nombreModulo"
+import notesManager from "./data/fs/NotesManager.js";
+
+//definimos un servidor para craerlo
+const server = express();
+
+//definimos el puerto a utilizar
+const port = 8080;
+
+//definimos la callback Ready para indicar que el servidor esta funcionando
+const ready = () => console.log("Server ready on port " + port);
+
+//levantamos el servir utilizando lisent
+server.listen(port, ready);
+
+//middlewares
+
+//obligo a mi servidor a usar la funcion encargada de leer parametros/consultas
+//permite leer req.params y query
+server.use(express.urlencoded({ extends: true }));
+
+//Configuramos el router
+
+server.get("/", async (requerimientos, respuestas) => {
+  try {
+    return respuestas.status(200).json({
+      response: "Corder API",
+      succes: true,
+    });
+  } catch (error) {
+    console.log(error);
+    return respuestas.status(500).json({
+      response: "Corder API Error",
+      succes: false,
+    });
+  }
+});
+
+//peticion con parametro
+
+//get para craecion
+server.get("/api/notes/:text/:category", async (req, res) => {
+  try {
+    const { text, category } = req.params;
+    const data = { text, category };
+    const one = await notesManager.create(data);
+    return res.status(201).json({
+      response: one,
+      succes: true,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      response: "ERROR",
+      succes: false,
+    });
+  }
+});
+
+//get para lectura
+
+server.get("/api/notes", async (req, res) => {
+  try {
+    const all = await notesManager.read();
+    return res.status(200).json({ response: all, succes: true });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      response: "ERROR",
+      success: false,
+    });
+  }
+});
+
+//buscar una nota con query
+
+server.get("/api/notes/category", async (req, res) => {
+  try {
+    const { category } = req.query;
+    const all = await notesManager.read(category);
+    if (all > 0) {
+      return res.status(200).json({ response: all, category, succes: true });
+    } else {
+      const error = new Error("NOT FOUND");
+      error.status = 404;
+      throw error;
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(error.status).json({
+      response: error.message,
+      succes: false,
+    });
+  }
+});
+
+//leer una nota
+server.get("/api/notes/:nid", async (req, res) => {
+  try {
+    const { nid } = req.params;
+    const one = await notesManager.readOne(nid);
+    if (one) {
+      return res.status(200).json({
+        response: one,
+        succes: true,
+      });
+    } else {
+      const error = new Error("NOT FOUND");
+      error.statusCode = 404;
+      throw error;
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(error.statusCode).json({
+      response: error.message,
+      succes: false,
+    });
+  }
+});
